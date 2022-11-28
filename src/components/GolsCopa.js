4afd446c92b32436f4c8aa7afcc8d28e3e67197c
WladiCosta <wladimircosta.dev@gmail.com>
2022-11-28 10:21:21 -0300

    Atualizado saldos de gols

--- src/components/GolsCopa.js
+++ src/components/GolsCopa.js
@@ -14,20 +14,23 @@ const selecoes2022 = selecoesCopa2022;
         <div >
           <ul className="selecs-2022">
              { selecoes2022.map((id, index) => (
+                <div className="container">
                  <div className="list">
                     <table>
-                        <tr  key={index}>
+                        <tr key={index}>
                             <td>{id.name}</td>
                             <td><span className="label">Gols Feitos:</span> <span className="score-scored">{id.goalScored}</span></td>
                             <td><span className="label">Gols Sofridos:</span> <span className="score-conceded">{id.goalConceded}</span></td>
                             {/* <td>Saldo: <span className="score">{id.goalScored - id.goalConceded}</span></td> */}
                             <td>
-                                {(id.goalScored - id.goalConceded) <= 0 ? (<><span className="label">Saldo:</span><span className="score-negative">{id.goalScored - id.goalConceded}</span></>) : 
+                                {(id.goalScored - id.goalConceded) <= 0 ? 
+                                (<><span className="label">Saldo:</span><span className="score-negative">{id.goalScored - id.goalConceded}</span></>) : 
                                 ( <><span className="label">Saldo:</span><span className="score-positive">{id.goalScored - id.goalConceded}</span></>)}
                             </td>
                         </tr>
                     </table>
                  </div>
+                </div>
              ))}
           </ul>
         </div>
